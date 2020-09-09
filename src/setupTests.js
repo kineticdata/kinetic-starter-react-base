5a3a9627326be2aab53338f39d3dddfe030b73fd
Matt Raykowski <matt.raykowski@gmail.com>
2020-09-09 15:27:55 -0500

    Fixed so Jest can run within the Bundle (and other apps.)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,15 +1,19 @@
-import 'babel-core/register';
 import 'babel-polyfill';
-import Enzyme from 'enzyme';
+import { configure, shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as matchers from 'jest-immutable-matchers';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 global.bundle = {
   apiLocation: () => '/acme/app/api/v1',
   spaceLocation: () => '/acme',
   kappSlug: () => 'queue',
 };
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
 
 beforeEach(() => jest.addMatchers(matchers));
