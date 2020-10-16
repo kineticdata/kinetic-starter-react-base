5b9fb899cfe7dcd915bd0eaeb43ed835c0d36013
marcinzarycki <marcin.zarycki@kineticdata.com>
2020-10-16 10:30:14 -0500

    Updated proxySetup to use proxyhelper from RKL

--- bundle/packages/app/src/setupProxy.js
+++ bundle/packages/app/src/setupProxy.js
@@ -1,34 +1,4 @@
-const removeSecure = cookie => cookie.replace(/;\s*Secure/i, '');
-const removeSameSiteNone = cookie => cookie.replace(/;\s*SameSite=None/i, '');
-
-const setupProxy = (target = process.env.REACT_APP_PROXY_HOST) => ({
-  target,
-  secure: true,
-  changeOrigin: true,
-  ws: true,
-  xfwd: true,
-  onProxyReq: proxyReq => {
-    // Browsers may send Origin headers even with same-origin
-    // requests. To prevent CORS issues, we have to change
-    // the Origin to match the target URL.
-    if (proxyReq.getHeader('origin')) {
-      proxyReq.setHeader('origin', target);
-    }
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    const setCookie = proxyRes.headers['set-cookie'];
-    if (setCookie && req.protocol === 'http') {
-      proxyRes.headers['set-cookie'] = Array.isArray(setCookie)
-        ? setCookie.map(removeSecure).map(removeSameSiteNone)
-        : removeSameSiteNone(removeSecure(setCookie));
-    }
-  },
-});
-
-// Not currently exported from @kineticdata/react, so the content of
-// proxyhelper is defined above instead.
-// TODO Uncomment and remove above code once proxyhelper can be imported.
-// const setupProxy = require('@kineticdata/react/proxyhelper');
+const setupProxy = require('@kineticdata/react/proxyhelper');
 
 const options = {
   ...setupProxy(),
