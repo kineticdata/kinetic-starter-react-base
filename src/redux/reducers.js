5bcb5d37ef14f4d204b6b4015c5f8e65d8161709
Matt Raykowski <matt.raykowski@gmail.com>
2021-03-03 08:44:58 -0600

    KP-2881: Implemented filestore API functions, FilestoreTable, and the list page. Made agent select a reusable component.

---
+++ src/redux/reducers.js
@@ -0,0 +1,15 @@
+import { reducer as about } from './modules/about';
+import { reducer as app } from './modules/app';
+import { reducer as alerts } from './modules/alerts';
+import { reducer as layout } from './modules/layout';
+import { reducer as profile } from './modules/profile';
+import { reducer as teams } from './modules/teams';
+
+export default {
+  about,
+  alerts,
+  app,
+  layout,
+  profile,
+  teams,
+};
