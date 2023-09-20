bf2504fb87bec6be465d25d998c4f84c10710bb4
marcinzarycki <marcin.zarycki@kineticdata.com>
2023-09-20 16:20:31 -0500

    Release v6.0.1
    Updated @kineticdata/* dependencies to latest v6.0.1 versions.
    Updated react to v17.

--- bundle/packages/app/src/setupTests.js
+++ bundle/packages/app/src/setupTests.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import { configure, shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import * as matchers from 'jest-immutable-matchers';
 
 configure({ adapter: new Adapter() });
