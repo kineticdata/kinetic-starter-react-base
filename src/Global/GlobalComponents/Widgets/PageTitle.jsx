ba20c532f3bebb1a6a25a41d71d2f15f50839163
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-03-12 08:50:12 -0500

    Adding the ability to have an element on the right side of the PageTitle comp then creating the docs link for the kapps page title right side.

--- src/Global/GlobalComponents/Widgets/PageTitle.jsx
+++ src/Global/GlobalComponents/Widgets/PageTitle.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 // PageTitle is meant to be used on pages within the app-content-container
-export const PageTitle = ({ title, subtext }) => (
+// The link argument should be passed in as it's own component.
+export const PageTitle = ({ title, subtext, rightSide }) => (
     <div className='page-title-wrapper'>
-        <div className="page-title">
-            {title}
-        </div>
-        {subtext && 
-            <div className="page-subtext">
-                {subtext}
+        <>
+            <div className="page-title">
+                {title}
             </div>
-        }
+            {subtext && 
+                <div className="page-subtext">
+                    {subtext}
+                </div>
+            }
+        </>
+        <>
+            {rightSide && rightSide}
+        </>
     </div>
 );
