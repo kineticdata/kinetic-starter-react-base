b3d93623cd74bc785519db1012287a9996ee1e44
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-03-14 11:26:46 -0500

    Removed unused imports

--- src/Global/GlobalComponents/GlobalPages/StandalonePages/LandingPage.jsx
+++ src/Global/GlobalComponents/GlobalPages/StandalonePages/LandingPage.jsx
@@ -1,7 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { GlobalContext } from '../../../GlobalResources/GlobalContextWrapper';
-import { LoadingSpinner } from '../../Widgets/LoadingSpinner';
 
 // LandingPage redirects to the kapps list by default,
 // update this component if a unique landing page is needed.
