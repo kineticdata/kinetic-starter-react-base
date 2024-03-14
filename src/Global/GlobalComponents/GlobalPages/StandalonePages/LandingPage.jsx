ba65dff589c12f86c78a94cbce49d0290d632cfe
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-03-14 10:30:36 -0500

    Comment change suggestions

--- src/Global/GlobalComponents/GlobalPages/StandalonePages/LandingPage.jsx
+++ src/Global/GlobalComponents/GlobalPages/StandalonePages/LandingPage.jsx
@@ -11,18 +11,4 @@ export const LandingPage = () => {
     useEffect(() => {
         navigate('/kapps')
     },[])
-    
-    // const globalState = useContext(GlobalContext);
-    // const { kineticSpace, userProfile, updateBreadcrumbs } = globalState;
-    
-    // useEffect(() => {
-    //     updateBreadcrumbs({ page: 'Home', path: '/'});
-    // }, [])
-    
-    // return kineticSpace && userProfile ? (
-    //     <div className='landing-page-wrapper'>
-    //         This is the base Landing Page, which is not initially turned on. <br />
-    //         To activate this page update the routing in App.js.
-    //     </div>
-    // ) : <LoadingSpinner />
 };          
