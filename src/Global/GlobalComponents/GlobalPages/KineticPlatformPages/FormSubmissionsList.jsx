2fb00a25c917db6e40dad1dc69035caa63e4cec3
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-03-26 09:12:35 -0500

    Added the submissions list table to the empty page. Cleanup includes: centered the loading spinner when its alone, fixed an error in the alphabetical sorting, fixed the form link on forms page, and changed the number on pagination to match the length if total length is less than the paginated amount.

--- src/Global/GlobalComponents/GlobalPages/KineticPlatformPages/FormSubmissionsList.jsx
+++ src/Global/GlobalComponents/GlobalPages/KineticPlatformPages/FormSubmissionsList.jsx
@@ -1,10 +1,115 @@
-import React from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { fetchForm, searchSubmissions, defineKqlQuery } from '@kineticdata/react';
+import { LoadingSpinner } from "../../Widgets/LoadingSpinner";
+import { PageTitle } from "../../Widgets/PageTitle";
+import { GlobalContext } from "../../../GlobalResources/GlobalContextWrapper";
+import { KineticTable } from "../../Widgets/KineticTable";
 
 export const FormSubmissionsList = () => {
+    const globalState = useContext(GlobalContext);
+    const { updateBreadcrumbs } = globalState;
+    const [ formsData, setFormsData ] = useState();
+    const [ submissionsData, setsubmissionsData ] = useState();
+    const { kappSlug, formSlug } = useParams();
 
-    return (
+    const columns = useMemo(() => {
+        return [{
+            title: 'Handle', 
+            value: 'handle', 
+            sortBy: 'string',
+        }, 
+        {
+            title: 'Label', 
+            value: 'label', 
+            sortBy: 'string',
+        },
+        {
+            title: 'State', 
+            value: 'state', 
+            sortBy: 'string',
+        }, 
+        {
+            title: 'Created at', 
+            value: 'createdAt', 
+            sortBy: 'date',
+        },
+        {
+            title: 'Updated at', 
+            value: 'updatedAt', 
+            sortBy: 'date',
+        }];
+    }) 
+
+    const getState = state =>  {
+        return (
+            <Link 
+                to={`forms/${formSlug}/submissions`} 
+                className="link"
+            >
+                View Form Submissions
+            </Link>
+        )
+    };
+
+    const getSubmissionLink = ( handle, submissionId ) => {
+        return (
+            <Link   
+                to={`${submissionId}`}
+                className="link"
+            >
+                {handle}
+            </Link>
+        )
+    };
+
+    useEffect(() => {
+        if(formsData) {
+            updateBreadcrumbs({ page: `${formsData.name} Submissions`, path: `/kapps/${kappSlug}/forms/${formSlug}/submissions`});
+        }
+    }, [formsData]);
+
+    useEffect(() => {
+        fetchForm({ kappSlug, formSlug, include: 'details' }).then(({ form }) => setFormsData(form));
+
+        const query = defineKqlQuery()
+            .end();
+
+        searchSubmissions({
+            kappSlug,
+            search: {
+                q: query({}),
+                include: ['details', 'values']
+            }
+            }).then(({ submissions }) => {
+                const parsedData = submissions.map(submission => ({
+                    handle: {
+                        toDisplay: getSubmissionLink(submission.handle, submission.id),
+                        toSort: submission.handle,
+                    },
+                    label: submission.label,
+                    state: {
+                        toDisplay: getState(submission.coreState),
+                        toSort: submission.coreState,
+                    },
+                    createdAt: {
+                        toDisplay: moment(submission.createdAt).format('MMMM Do, YYYY - h:mm:ss a'),
+                        toSort: submission.createdAt,
+                    },
+                    updatedAt: {
+                        toDisplay: moment(submission.updatedAt).format('MMMM Do, YYYY - h:mm:ss a'),
+                        toSort: submission.updatedAt,
+                    },
+                }));
+                setsubmissionsData(parsedData);
+            }
+        );
+    }, [kappSlug, formSlug]);
+
+    return formsData && submissionsData ? (
         <>
-            This is the Form Submissions Page.
+            <PageTitle title={`${formsData.name} Submissions`} />
+            <KineticTable columns={columns} data={submissionsData} showPagination />
         </>
-    )
+    ) : <LoadingSpinner />
 };
