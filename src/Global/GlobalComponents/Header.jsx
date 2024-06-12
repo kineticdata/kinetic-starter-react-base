8e4e40cc5369c98ba660f9c3cfa955fc2040f490
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-06-12 12:30:19 -0500

    Styling updates

--- src/Global/GlobalComponents/Header.jsx
+++ src/Global/GlobalComponents/Header.jsx
@@ -63,7 +63,7 @@ const profileDropdownHeader = useMemo(() => ( userProfile &&
   
   return (
     <div className='header-containter'>
-      <Link to="/">
+      <Link to="/" className='header-logo'>
         <img
           alt="logo"
           src={logo}
@@ -79,7 +79,6 @@ const profileDropdownHeader = useMemo(() => ( userProfile &&
             dropdownFace={
               <button 
                 aria-label='Help Menu'
-                className='header-dropdown-links'
                 onClick={() => {
                     setIsProfileMenuOpen(false);
                     setIsHelpMenuOpen(!isHelpMenuOpen);
