0d6688d88f7c06a2d459bc5ac2b822b35547edaf
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-06-26 17:09:59 -0400

    icon troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -8,27 +8,6 @@ export default defineConfig(({ command, mode }) => {
   return {
     build: {
       outDir: 'build',  
-      extendViteConf(viteConf) {
-        if (viteConf.css !== Object(viteConf.css)) {
-          viteConf.css = {};
-        }
-        if (viteConf.css.postcss !== Object(viteConf.css.postcss)) {
-          viteConf.css.postcss = {};
-        }
-        if (Array.isArray(viteConf.css.postcss.plugins) === false) {
-          viteConf.css.postcss.plugins = [];
-        }
-        viteConf.css.postcss.plugins.unshift({
-          postcssPlugin: 'internal:charset-removal',
-          AtRule: {
-            charset: (atRule) => {
-              if (atRule.name === 'charset') {
-                atRule.remove();
-              }
-            },
-          },
-        });
-      },
     },
     esbuild: {
       loader: 'jsx',
@@ -62,6 +41,22 @@ export default defineConfig(({ command, mode }) => {
     },
     define: {
       'process.env': env,
-    }
+    },
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
+      }
+  }
   };
 })
