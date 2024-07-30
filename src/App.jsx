91b2fc3825decf5240c9a0395b6b19234b2c7425
alexafleming <111538729+alexafleming@users.noreply.github.com>
2024-07-30 14:27:58 -0500

    Testing Removing CoreForms

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-import './Global/Assets/Styles/master.scss'
 import './Global/Assets/Styles/master.css'
 import { Route, Routes } from 'react-router-dom';
 import { LoadingSpinner } from './Global/GlobalComponents/Widgets/LoadingSpinner';
