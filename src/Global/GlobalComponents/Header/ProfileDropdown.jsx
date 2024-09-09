f83f17b130104a1f4b832b8c917b4d2ef1766143
Spencer Faust <spencer@Spencers-MacBook-Pro.local>
2024-09-09 16:54:43 -0500

    Upgraded from @kineticdata/react  6.0.0 to 6.0.5. Also updated the Sign Out button to appropriatelly log the user out and redirect to the login page.

--- src/Global/GlobalComponents/Header/ProfileDropdown.jsx
+++ src/Global/GlobalComponents/Header/ProfileDropdown.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { logout } from '@kineticdata/react';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
@@ -7,61 +8,81 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 
-export const ProfileDropdown = ({profile, openModal, closeProfileAnchor}) => (
-  <Box sx={{ p: '.5rem 1rem' }}>
-    <Box
-      sx={{
-        width: '19.875rem',
-        height: '4.625rem',
-        bgcolor: 'primary.quaternary',
-        borderRadius: '.5rem',
-        mb: '2.375rem',
-      }}
-    >
-      <Avatar
-        variant="circular"
+export const ProfileDropdown = ({profile, openModal, closeProfileAnchor}) => {
+  const navigate = useNavigate();
+  console.log('OPE', navigate)
+
+  return (
+    <Box sx={{ p: '.5rem 1rem' }}>
+      <Box
         sx={{
-          bgcolor: 'secondary.secondary',
-          height: '4.563rem',
-          width: '4.563rem',
-          position: 'absolute',
-          top: '3.35rem',
-          left: '9rem',
-          fontSize: '2.25rem',
+          width: '19.875rem',
+          height: '4.625rem',
+          bgcolor: 'primary.quaternary',
+          borderRadius: '.5rem',
+          mb: '2.375rem',
         }}
       >
-        {profile.displayName[0]}
-      </Avatar>
-    </Box>
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        gap: '1rem',
-        pt: '1rem',
-      }}
-    >
+        <Avatar
+          variant="circular"
+          sx={{
+            bgcolor: 'secondary.secondary',
+            height: '4.563rem',
+            width: '4.563rem',
+            position: 'absolute',
+            top: '3.35rem',
+            left: '9rem',
+            fontSize: '2.25rem',
+          }}
+        >
+          {profile.displayName[0]}
+        </Avatar>
+      </Box>
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'row',
           alignItems: 'center',
-          gap: '1.5rem',
+          flexDirection: 'column',
+          gap: '1rem',
+          pt: '1rem',
         }}
       >
-        <Typography sx={{ fontWeight: '800', fontSize: '1.5rem' }}>
-          {profile.displayName}
-        </Typography>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="profile"
-          onClick={() => {
-            openModal();
-            closeProfileAnchor();
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '1.5rem',
           }}
+        >
+          <Typography sx={{ fontWeight: '800', fontSize: '1.5rem' }}>
+            {profile.displayName}
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="profile"
+            onClick={() => {
+              openModal();
+              closeProfileAnchor();
+            }}
+            sx={{
+              color: 'primary.secondary',
+              fontWeight: 'bold',
+              '&:hover': {
+                color: 'primary.main',
+                backgroundColor: 'primary.quaternary',
+              },
+            }}
+          >
+            <EditIcon />
+          </IconButton>
+        </Box>
+        <Typography>{profile.email}</Typography>
+        <Button
+          variant="text"
+          onClick={() => logout(() => navigate('/'))}
           sx={{
             color: 'primary.secondary',
             fontWeight: 'bold',
@@ -71,25 +92,9 @@ export const ProfileDropdown = ({profile, openModal, closeProfileAnchor}) => (
             },
           }}
         >
-          <EditIcon />
-        </IconButton>
+          Sign Out
+        </Button>
       </Box>
-      <Typography>{profile.email}</Typography>
-      <Button
-        variant="text"
-        href="/"
-        onClick={logout}
-        sx={{
-          color: 'primary.secondary',
-          fontWeight: 'bold',
-          '&:hover': {
-            color: 'primary.main',
-            backgroundColor: 'primary.quaternary',
-          },
-        }}
-      >
-        Sign Out
-      </Button>
     </Box>
-  </Box>
-);
+  )
+};
